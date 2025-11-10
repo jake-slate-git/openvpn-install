b42fbda2903984ee5a404362677d1f3dbad17661
jake-slate-git <jacob@slateintegrations.com>
2025-11-10 15:59:42 -0800

    Update testinstall10.sh

--- testinstall10.sh
+++ testinstall10.sh
@@ -4,10 +4,12 @@ sudo mkdir -p /opt/openvpn-as/ovpn_files/client_files
 
 sudo chmod +x openvpn-install/* 
 
-sudo ./openvpn-install/headless-openvpn-install-10-clients.sh
+cd openvpn-install/
 
-sudo ./openvpn-install/TLS_client_removal.sh
+sudo ./headless-openvpn-install-10-clients.sh
 
-sudo ./openvpn-install/bridge_setup.sh
+sudo ./TLS_client_removal.sh
+
+sudo ./bridge_setup.sh
 
 ls /opt/openvpn-as/ovpn_files/client_files/
