3eda641f91e05348df422ece3314ac063b4f77cc
jake-slate-git <jacob@slateintegrations.com>
2025-11-10 22:14:09 -0800

    Update client files to include script-security setting
    Add script-security 2 setting to client configuration files.

--- TLS_client_removal.sh
+++ TLS_client_removal.sh
@@ -6,4 +6,9 @@ for f in *; do
     "$f"
 done
 
+echo "adding script-security 2 setting to client files" 
+for f in /opt/openvpn-as/ovpn_files/client_files/*.ovpn; do
+    sudo grep -q '^script-security' "$f" || echo 'script-security 2' | sudo tee -a "$f" >/dev/null
+done
+
 cd
